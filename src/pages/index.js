03dee71f385860dc7fafcc6a505975a9d2fcf812
zaycationdev <isaiahthomas098@gmail.com>
2022-01-01 03:16:26 -0800

    updated bg image and bg color w/ text

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,34 +1,38 @@
 import React, { useEffect } from "react"
 import { graphql } from "gatsby"
+import Image from "react-bootstrap/Image"
 import "bootstrap/dist/css/bootstrap.min.css"
+import "./mainpage.css"
 
 const IndexPage = ({ data }) => {
   // const { edges } = data.allMarkdownRemark
   useEffect(() => {
-    window.open("https://discord.com/invite/n6YcyNm9sb", "_self")
+    setInterval(() => {
+      window.open("https://discord.com/invite/n6YcyNm9sb", "_self")
+    }, 3000)
   }, [])
   return (
     <>
-      <div>
+      <div className="mainpagebg">
         <h3
           style={{
+            color: "white",
             textAlign: "center",
-            paddingTop: "15rem",
+            paddingTop: "8rem",
             textDecoration: "underline",
             textDecorationColor: "purple",
           }}
         >
           Taking you to our discord!
         </h3>
-        <h5
-          style={{
-            textAlign: "center",
-            paddingTop: "2rem",
-          }}
-        >
-          This will just take a second or two..
+        <h5 style={{ color: "white", textAlign: "center", paddingTop: "2rem" }}>
+          This will on take a second..
         </h5>
-        
+
+        <Image
+          src="https://cdn.discordapp.com/attachments/817491642655047751/890092074827927562/beyondcult.gif"
+          fluid
+        />
       </div>
     </>
   )
